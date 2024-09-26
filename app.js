d242cbee22159147fc7f81023586c29183afd52a
chryschi <121822197+chryschi@users.noreply.github.com>
2024-09-26 18:01:57 +0200

    Show dropdown menu button for every track

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const moodsRouter = require("./routes/moodsRouter");
 
 const app = express();
 
+app.use("/assets", express.static("assets"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/tracks", trackRouter);
