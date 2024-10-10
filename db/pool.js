e9cc9cd67a3d5e76994756004f90f93818173471
chryschi <121822197+chryschi@users.noreply.github.com>
2024-10-10 15:54:59 +0200

    Add connection to remote database

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,25 @@
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
   port: process.env.PORTDB,
+  ssl: {
+    require: true,
+  },
 });
+
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query("SELECT version()");
+    console.log(result.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+getPgVersion();
+
+module.exports = pool;
